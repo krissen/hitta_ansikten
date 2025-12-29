01b5debdf8b123c32d1ab564a28b8967608d0d2d
Kristian Niemi <kristian.niemi@gmail.com>
2025-12-29 13:53:38 +0100

    (fix) Add Edit menu with copy/paste/cut roles for DevTools clipboard support
    - Added standard Edit menu with Undo/Redo/Cut/Copy/Paste/SelectAll
    - Uses Electron built-in roles that work in all contexts including DevTools
    - Enables Cmd+C/Cmd+V/Cmd+X in DevTools console

--- frontend/src/main/menu.js
+++ frontend/src/main/menu.js
@@ -64,6 +64,44 @@ function createApplicationMenu(mainWindow) {
       ]
     }] : []),
 
+    // Edit menu (needed for copy/paste in DevTools)
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo'
+        },
+        { type: 'separator' },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectAll'
+        }
+      ]
+    },
+
     // File menu
     {
       label: 'File',
